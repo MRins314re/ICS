beff1306d74f09fe34259cb87f2b56f5aa966e32
MRins314re <mstudenny@huronperthcatholic.ca>
2024-09-06 13:17:36 -0400

    Add files via upload

---
+++ helloWorldStarter.py
@@ -0,0 +1,8 @@
+#Any text that is green and contained with a '#' symbol is a comment and will be ignored when the program is ran
+#The first command we are learning in this course is called "print". 
+#The first use of print will be to print strings. 
+#Inside the parenthesis and quotation marks below type the text Hello World!
+print("Hello World!")
+#Now hit f5 to run this program.
+#When you see it successfully run in the terminal delete all comments and rename the file to helloWorld
+#Then you will submit the .py file as your first assessment!
